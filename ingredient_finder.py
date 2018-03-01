021f35765995573bbed757349463d9c3c887c174
Yunil Seo <yunilseo@dhcp-10-105-150-98.wireless.northwestern.private>
2018-02-28 23:07:57 -0600

    getting ingredients back from the main page, Dylan Ong

--- ingredient_finder.py
+++ ingredient_finder.py
@@ -9,37 +9,35 @@ def get_recipe(url):
 	soup = BeautifulSoup(page, 'html.parser')
 	return soup
 
+# def getNameFromHandle(handle):
+# 	try:
+# 		url = 'https://api.twitter.com/1.1/users/lookup.json?screen_name=' + handle
+# 		auth = OAuth1(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# 		r = requests.get(url, auth=auth)
+# 		return json.loads(r.text)[0]['name']
+# 	except:
+# 		return None
+
 def get_urls(html):
 	url_set = set()
 	url_list_1 = html.find('section', {'id': 'grid'})
 	for link in url_list_1.findAll('a', href=True):
-		if '/recipe/' not in link['href'] and 'https://www.allrecipes.com' not in link['href']:
-			continue
-		else:
+		if '/recipe/' in link['href'] and 'https://www.allrecipes.com' in link['href']:
 			url_set.add(link['href'])
+		else:
+			continue
 	return url_set
 
-def get_ingredients(html):
-	#ingredients broken into 2 drop down lists from html
-	ingredients = []
-	ingredients_list_1 = html.find('ul', {'class': 'checklist dropdownwrapper list-ingredients-1'})
-	ingredients_list_2 = html.find('ul', {'class': 'checklist dropdownwrapper list-ingredients-2'})
-	ingredients_list_1 = ingredients_list_1.findAll('li', {'class': 'checkList__line'})
-	ingredients_list_2 = ingredients_list_2.findAll('li', {'class': 'checkList__line'})
-	list_of_ingredients = ingredients_list_1 + ingredients_list_2
-	for item in list_of_ingredients:
-		ingredients.append(str(item.contents[1].text).strip())
-	return ingredients[0:len(ingredients)-1]
+#https://www.allrecipes.com/search/results/?wt=mexican&sort=re&page=6#6
 
 def main():
 	ingredients_list = []
-	urls = get_urls(get_recipe('https://www.allrecipes.com/?page=2#2'))
-	print(urls)
-	# for url in urls:
-	# 	html = get_recipe(url)
-	# 	ingredients = get_ingredients(html)
-	# 	ingredients_list.append(ingredients)
-	# print(ingredients_list)
+	urls = get_urls(get_recipe('https://www.allrecipes.com/?page=2'))
+	for url in urls:
+		html = get_recipe(url)
+		ingredients = get_ingredients(html)
+		ingredients_list.append(ingredients)
+	print(ingredients_list)
 
 main()
 
