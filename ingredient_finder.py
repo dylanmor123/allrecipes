afee17f56334b79dd1fe33936cbebc497629b9ee
Yunil Seo <yunilseo@dhcp-10-105-6-43.wireless.northwestern.private>
2018-03-01 16:52:50 -0600

    cleaned up

--- ingredient_finder.py
+++ ingredient_finder.py
@@ -1,16 +1,6 @@
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-from allrecipes_scraper import get_ingredients as get_ingredients
-import operator as operator
+from allrecipes_scraper import get_ingredients, get_recipe
 import time
 import argparse
-
-def get_recipe(url):
-	hdr = {'User-Agent': 'Mozilla/5.0'}
-	req = Request(url, headers=hdr)
-	page = urlopen(req)
-	soup = BeautifulSoup(page, 'html.parser')
-	return soup
 
 def getAllIngredients(urls):
 	ingredient_lists = []
