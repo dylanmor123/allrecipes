dc4824b6db21ca2bfb4b10868c56238ca1097f45
Danilo <daniloneves.web@gmail.com>
2018-03-01 00:28:50 -0600

    Danilo: add methods to knowledge base

--- knowledgebase.py
+++ knowledgebase.py
@@ -1,8 +1,11 @@
 # This is a data structure with representing our knowledge base.
 
-# NOTE: If you are adding a new fields (e.g. name) make sure to
+# NOTE 1: If you are adding new fields (e.g. name) make sure to
 #   update all the relevant entries and try to make the field name
 #   self explanatory.
+
+# NOTE 2: Make sure the code is not broken after modifications! 
+#   Make sure to check open and closing brackets
 
 KNOWLEDGE_BASE = {
     "ingredients": [
@@ -35,8 +38,101 @@ KNOWLEDGE_BASE = {
     ],
     "methods": [
         {
-            "name": "Sauté",
-            "description": 
-        },    
+            "name": "Baking",
+            "sub-groups": [
+                {
+                    "name": "Shirring",
+                },
+            ],
+            "Dry": "yes",
+        }, 
+        {
+            "name": "Broiling",
+            "Dry": "yes",
+        },
+        {
+            "name": "Frying",
+            "sub-groups": [
+                {
+                    "name": "Deep fat frying",
+                },
+                {
+                    "name": "Sautéing",
+                },
+                {
+                    "name": "Stir-Frying",
+                },
+                {
+                    "name": "Deglazing",
+                },
+            ],
+            "Dry": "yes",
+        },
+        {
+            "name": "Grilling",
+            "Dry": "yes",
+        },
+        {
+            "name": "Bain-marie",
+            "Wet": "yes",
+        },
+        {
+            "name": "Basting",
+            "Wet": "yes",
+        },
+        {
+            "name": "Blanching",
+            "Wet": "yes",
+        },
+        {
+            "name": "Boiling",
+            "Wet": "yes",
+        },
+        {
+            "name": "Clay pot cooking",
+            "Wet": "yes",
+        },
+        {
+            "name": "Poaching",
+            "Wet": "yes",
+        },
+        {
+            "name": "Pressure cooking",
+            "Wet": "yes",
+        },
+        {
+            "name": "Scalding",
+            "Wet": "yes",
+        },
+        {
+            "name": "Simmering",
+            "Wet": "yes",
+        },
+        {
+            "name": "Sous-vide",
+            "Wet": "yes",
+        },
+        {
+            "name": "Steaming",
+            "Wet": "yes",
+        },
+        {
+            "name": "Stewing",
+            "Wet": "yes",
+        },
+        {
+            "name": "Tempering",
+            "Wet": "yes",
+        },
+        {
+            "name": "Thermal Cooking",
+            "Wet": "yes",
+        },
+        {
+            "name": "Caramelizing",
+            "Wet": "yes",
+        },
     ],
 }
+
+print(KNOWLEDGE_BASE)
