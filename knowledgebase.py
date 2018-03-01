cb6c9ae54f01c3c141f905811da6176d39af4ba9
Danilo <daniloneves.web@gmail.com>
2018-02-28 22:16:06 -0600

    Danilo: update knowledge base

--- knowledgebase.py
+++ knowledgebase.py
@@ -1,3 +1,42 @@
-List of Ingredients
-List of Vegan Ingredients
-List of Healthy Ingredients
+# This is a data structure with representing our knowledge base.
+
+# NOTE: If you are adding a new fields (e.g. name) make sure to
+#   update all the relevant entries and try to make the field name
+#   self explanatory.
+
+KNOWLEDGE_BASE = {
+    "ingredients": [
+        {
+            "name": "milk",
+            "sub-groups": [
+                {
+                    "name": "cow milk",
+                    "vegan": "no",
+                },
+                {
+                    "name": "soy milk",
+                    "vegan": "yes",
+                },
+                {
+                    "name": "rice milk",   
+                    "vegan": "yes",
+                },
+                {
+                    "name": "oat milk",
+                    "vegan": "yes",
+                },
+            ]
+        },
+    ],
+    "tools": [
+        {
+            "name": "pot",
+        },
+    ],
+    "methods": [
+        {
+            "name": "Sauté",
+            "description": 
+        },    
+    ],
+}
