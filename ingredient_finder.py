6963089985081a3a814e09e01254d3a1dbffae96
Yunil Seo <yunilseo@dhcp-10-105-6-43.wireless.northwestern.private>
2018-03-01 16:47:58 -0600

    completed retrieving ingredients given user input

--- ingredient_finder.py
+++ ingredient_finder.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 from allrecipes_scraper import get_ingredients as get_ingredients
+import operator as operator
+import time
+import argparse
 
 def get_recipe(url):
 	hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -9,15 +12,33 @@ def get_recipe(url):
 	soup = BeautifulSoup(page, 'html.parser')
 	return soup
 
-# def getNameFromHandle(handle):
-# 	try:
-# 		url = 'https://api.twitter.com/1.1/users/lookup.json?screen_name=' + handle
-# 		auth = OAuth1(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-# 		r = requests.get(url, auth=auth)
-# 		return json.loads(r.text)[0]['name']
-# 	except:
-# 		return None
+def getAllIngredients(urls):
+	ingredient_lists = []
+	for url in urls:
+		html = get_recipe(url)
+		ingredients = get_ingredients(html)
+		ingredient_lists.append(ingredients)
+		time.sleep(1)
+	return(ingredient_lists)
 
+def getIngredientsFromInput(cuisine, page_counter):
+	counter = 0
+	total_urls = set()
+	while counter < page_counter+1:
+		try:
+			url = 'https://www.allrecipes.com/search/results/?wt=' + cuisine + '&sort=re&page=' + str(counter)
+			html = get_recipe(url)
+			urls = get_urls(html)
+			total_urls = total_urls|urls
+			counter += 1
+		except:
+			print("link is broken")
+			return None
+	ingredient_lists = getAllIngredients(total_urls)
+	print(ingredient_lists)
+	print("Number of recipes: ", len(ingredient_lists))
+	return ingredient_lists
+	
 def get_urls(html):
 	url_set = set()
 	url_list_1 = html.find('section', {'id': 'grid'})
@@ -28,18 +49,12 @@ def get_urls(html):
 			continue
 	return url_set
 
-#https://www.allrecipes.com/search/results/?wt=mexican&sort=re&page=6#6
+def main(cuisine_style, count):
+    getIngredientsFromInput(cuisine_style, count)
 
-def main():
-	ingredients_list = []
-	urls = get_urls(get_recipe('https://www.allrecipes.com/?page=2'))
-	for url in urls:
-		html = get_recipe(url)
-		ingredients = get_ingredients(html)
-		ingredients_list.append(ingredients)
-	print(ingredients_list)
-
-main()
-
-
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cuisine_style', type=str) 
+    parser.add_argument('count', type=int)
+    args = parser.parse_args()
+    main(args.cuisine_style, args.count)
