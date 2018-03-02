059374e6869836022565dd4b503b535e0caf8c84
M. Smith <animaniac21@gmail.com>
2018-03-01 23:12:47 -0600

    concurrent page grabbing : michael yunil

---
+++ ingredient_finder_parallel.py
@@ -0,0 +1,67 @@
+import allrecipes_scraper as ars
+import time
+import argparse
+from multiprocessing import Pool
+
+
+
+def getAllRecipeData(urls):
+	recipe_lists = []
+	for url in urls:
+		recipe = ars.create_recipe_data(url)
+		time.sleep(1)
+		recipe_lists.append(recipe)
+	print(recipe_lists)
+	print(len(recipe_lists))
+	return(recipe_lists)
+
+def helper(pair):
+	#print(pair[0], pair[1])
+	try:
+		url = 'https://www.allrecipes.com/search/results/?wt=' + str(pair[1]) + '&sort=re&page=' + str(pair[0])
+		html = ars.get_recipe(url)
+		urls = get_urls(html)
+		return urls
+	except:
+		print("link is broken")
+		print(url)
+		return None
+
+
+def getIngredientsFromInput(cuisine, page_counter):
+	counter = 0
+	#total_urls = set()
+	p = Pool(page_counter)
+	total_urls = p.map(helper, map(lambda num: [num, cuisine], list(range(1, page_counter+1))))
+	print(len(total_urls))
+	url_list = [url for sublist in total_urls for url in sublist]
+	print(url_list)
+
+	recipe_lists = getAllRecipeData(url_list)
+	print(recipe_lists)
+	print("Number of recipes: ", len(recipe_lists))
+	f = open("{}_recipes.txt".format(cuisine), "w+")
+	for recipe in recipe_lists:
+		f.write("%s\n" % recipe)
+	f.close()
+	return recipe_lists
+	
+def get_urls(html):
+	url_set = set()
+	url_list_1 = html.find('section', {'id': 'grid'})
+	for link in url_list_1.findAll('a', href=True):
+		if '/recipe/' in link['href'] and 'https://www.allrecipes.com' in link['href']:
+			url_set.add(link['href'])
+		else:
+			continue
+	return url_set
+
+def main(cuisine_style, count):
+    getIngredientsFromInput(cuisine_style, count)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cuisine_style', type=str) 
+    parser.add_argument('count', type=int)
+    args = parser.parse_args()
+    main(args.cuisine_style, args.count)
