8caab948a9780ec62a4680ad84d1cc1244267ede
M. Smith <animaniac21@gmail.com>
2018-03-03 20:54:17 -0600

    hybrid method : michael

--- ingredient_finder_parallel.py
+++ ingredient_finder_parallel.py
@@ -4,14 +4,25 @@ import argparse
 from multiprocessing import Pool
 
 
+def chunks(l, n):
+	for i in range(0, len(l), n):
+		yield l[i:i + n]
+
 def getAllRecipeData(urls):
 	recipe_lists = []
-	for url in urls:
-		recipe = ars.create_recipe_data(url)
-		time.sleep(1)
-		recipe_lists.append(recipe)
-	print(recipe_lists)
-	print(len(recipe_lists))
+	GROUP_SIZE = 4
+
+	groups = list(chunks(urls, GROUP_SIZE))
+
+	for group in groups:
+		print(group[0])
+
+		p = Pool(GROUP_SIZE)
+		recipes = p.map(ars.create_recipe_data, group)  # ars.create_recipe_data(url)
+		#print(recipes)
+		time.sleep(2)
+		recipe_lists.append(recipes)
+	#print(len(recipe_lists))
 	return(recipe_lists)
 
 
@@ -29,23 +40,38 @@ def helper(pair):
 
 
 def getIngredientsFromInput(cuisine, page_counter):
-	# total_urls = set()
-	p = Pool(page_counter)
-	total_urls = p.map(helper, map(lambda num: [num, cuisine], list(range(1, page_counter + 1))))
+	# # total_urls = set()
+	# p = Pool(page_counter)
+	# total_urls = p.map(helper, map(lambda num: [num, cuisine], list(range(1, page_counter + 1))))
+	# print(len(total_urls))
+	# url_list = [url for sublist in total_urls for url in sublist]
+	# print(len(url_list))
+
+	counter = 1
+	total_urls = set()
+	while counter < page_counter+1:
+		try:
+			url = 'https://www.allrecipes.com/search/results/?wt=' + cuisine + '&sort=re&page=' + str(counter)
+			html = ars.get_recipe(url)
+			urls = get_urls(html)
+			total_urls = total_urls|urls
+			counter += 1
+		except:
+			print("link is broken")
+			print(url)
+			return None
+
 	print(len(total_urls))
-	url_list = [url for sublist in total_urls for url in sublist]
-	#print(url_list)
+	recipe_lists = getAllRecipeData(list(total_urls))
+	final_list = [rec for sublist in recipe_lists for rec in sublist]
+	# p2.map(ars.create_recipe_data, url_list)
 
-	p2 = Pool(len(url_list))
-	recipe_lists = p2.map(ars.create_recipe_data, url_list)
-
-	#print(recipe_lists)
-	print("Number of recipes: ", len(recipe_lists))
+	print("Number of recipes: ", len(final_list))
 	f = open("{}_recipes.txt".format(cuisine), "w+")
-	for recipe in recipe_lists:
+	for recipe in final_list:
 		f.write("%s\n" % recipe)
 	f.close()
-	return recipe_lists
+	return final_list
 
 
 def get_urls(html):
