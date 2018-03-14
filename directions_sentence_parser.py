5f9b7c11e2cf1a5b301c6c6caf6b0208d0fcbfe3
Dylan Ong <Dylan Ong>
2018-03-13 22:58:52 -0500

    almost finished with directions parser - dylan ong

--- directions_sentence_parser.py
+++ directions_sentence_parser.py
@@ -12,93 +12,62 @@ def load_recipes(file):
 	return dicts_from_file 
 
 
-
-subtree = getKBSubtree(['cooking-methods'])
-cooking_methods = ' '.join(list(subtree.keys()))
-recipe = load_recipes('chinese_recipes.txt')[0]
-
-BLACKLISTED_WORDS = ['large', 'small', ]
-
-# "name": "recipe name",
-# 	"description": "recipe description",
-# 	"ingredients":  [{  
-# 	    "name":    "sea salt",  
-# 	    "quantity":    1,  
-# 	    "measurement":    "pinch",
-# 	    "preparation":    "none"  
-# 	}],
-# 	"directions" : [{
-# 		"raw_text": "Task 1. Task 2."
-# 		"sentences": [{
-# 			"raw_text" : "Task 1."
-# 			"cooking methods" : ["bake"]
-# 			"ingredients_from_list" : [{
-#                                         "ingredient_list_index": 1,
-#                                         "sentence_spam": (3, 4)
-#                                     }],
-#                                     "ingredients_from_kb" : ["yolk", "whites"]
-# 			"tools" : ["spoon", "oven"]
-# 			"action_verbs" : ["crack", "pour"]
-# 			"duration" : "3 to 5 minutes"
-# 		}]
-# 	}],
-#     "cooking method": "primary cooking method",  
-# 	"cooking tools": ["knife", "grater", "dutch oven"],    
-# 	"nutrition_facts": [{
-# 		"quantity": "104.8",
-# 		"measurement": "g",
-# 		"name": "carbohydrates"
-# 	},],
-
-def directions_sentence_parser(recipe):
-	cooking_methods = []
-	final_sentences = []
+def directions_parser(recipe):
+	# Get ingredient names from recipe
 	list_of_ingredients = []
+	all_sentences = []
 	for ingredient in recipe['ingredients']:
 		list_of_ingredients.append(ingredient['name'])
-	for direction in recipe['directions']:
-		tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-		data = direction
-		sentences = tokenizer.tokenize(data)
-		for s in range(0, len(sentences)):
-			raw_text = sentences[s]
-			sentence = sentences[s].replace("-", ".").lower()
-			cooking_methods = get_regex_match_directions(sentence, list(getKBSubtree(["cooking-methods"]).keys()), 'method')
-			ingredients_from_kb = get_regex_match_directions(sentence, list(getKBSubtree(["ingredients"]).keys()), 'ingredient')
-			duration = get_duration(sentence)
-			ingredients_from_list = []
-			tokens = ''.join(c for c in sentence if c not in ('!','.',':', ',', ';')).split()
+
+	sentences = get_sentences(' '.join(recipe['directions']))
+
+	for sentence in sentences:
+		raw_text = sentence
+		sentence = sentence.replace("-", ".").lower()
+
+		duration = get_duration(sentence)
+
+		# TODO
+		# action_verbs = match_from_kb(sentence, list(getKBSubtree(["cooking-actions"]).keys()), 'verb')
+
+		cooking_methods = match_from_kb(sentence, list(getKBSubtree(["cooking-methods"]).keys()), 'verb')
+		tools = match_from_kb(sentence, list(getKBSubtree(["tools"]).keys()), 'noun')
+
+		ingredients = []
+		kb_ingredients = match_from_kb(sentence, list(getKBSubtree(["ingredients"]).keys()), 'noun')
+
+		for ingredient in kb_ingredients:
+			match_ingredient_list_indices = []
 			for i in range(0, len(list_of_ingredients)):
-				ingredient_matches = get_regex_match_directions(list_of_ingredients[i], tokens, 'ingredient')
-				if ingredient_matches:
-					print(tokens)
-					print(list_of_ingredients, ingredient_matches)
-					print('\n')
-				ingredient_number = i
-			step = {'raw_text': raw_text,
-					'ingredients_from_kb': ingredients_from_kb,
-					'ingredients_from_list': ingredients_from_list,
-					'cooking_methods': cooking_methods,
-					'tools': 'TODO ADD TOOLS',
-					'actions': 'TODO ADD NON-COOKING PREPARATION METHODS',
-					'duration': duration
-					}
-			final_sentences.append(step) 
-	recipe['directions'] = {'raw_text': recipe['directions'], 'sentences': final_sentences}
-	return recipe
+				if match_noun_in_sentence(list_of_ingredients[i], ingredient) or match_noun_in_sentence(ingredient, list_of_ingredients[i]):
+					match_ingredient_list_indices.append(i)
+			ingredients.append({'name': ingredient, 'match_ingredient_list_indices': match_ingredient_list_indices})
+		print(ingredients)
+		all_sentences.append(
+							{'raw_text': raw_text,
+							'ingredients': ingredients,
+							'cooking_methods': cooking_methods,
+							'tools': tools,
+							# Need to add all non-cooking techniques to knolwedge base
+							# For now, just use the first word
+							'action_verbs': sentence.split()[0],
+							'duration': duration
+			})
+	return all_sentences
 
 
-def get_regex_match_directions(sentence, kb_subtree, query = 'ingredient'):
+def match_from_kb(sentence, kb_items, query):
 	matches_in_sentence = []
-	for item in kb_subtree:
-		if query == 'method':
-			item_match = match_verb_in_sentence(item, sentence)
-		else:
+	for item in kb_items:
+		item_match = []
+		if query == 'noun':
 			item_match = match_noun_in_sentence(item, sentence)
-		if len(item_match) > 0:
-			matches_in_sentence.append(item)
-
+		if query == 'verb':
+			item_match = match_verb_in_sentence(item, sentence)
+		if item_match:
+			matches_in_sentence.append(item_match[0][1])
 	return matches_in_sentence
+
 
 
 def match_noun_in_sentence(word, sentence):
@@ -115,12 +84,16 @@ def get_duration(sentence):
 			duration += sentence.split(unit)[0].split()[-1] + ' ' + unit + 's '
 	return duration.strip()
 
-print(get_duration('here is a sample of 4 minutes and 45 hours you know'))
+def get_sentences(directions):
+	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+	return tokenizer.tokenize(directions)
 
-methods = set([''])
-methods = directions_sentence_parser(recipe)
-print(methods['directions'])
-# for word in methods:
-# 	print(word) 
-	
-# print(methods)
+
+
+def main():
+	recipe = load_recipes('chinese_recipes.txt')[0]
+	directions = directions_parser(recipe)
+	for direction in directions:
+		print(direction)
+
+main()
