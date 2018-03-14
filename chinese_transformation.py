bdfdc5e9cf36428c50a78946681460463ca888d1
M. Smith <animaniac21@gmail.com>
2018-03-13 23:12:26 -0500

    transforms and scripts: michael

--- chinese_transformation.py
+++ chinese_transformation.py
@@ -70,6 +70,7 @@ def transform(recipe, style="chinese"):
 	subs = {}
 	for ingredient in recipe["ingredients"]:
 		if ingredient["name"].lower() in list(subtree.keys()):
+			print("found", ingredient["name"].lower())
 			ingr_tags = list(getKBSubtree(["ingredients", ingredient["name"].lower()]))
 
 			if style not in ingr_tags:
@@ -84,16 +85,44 @@ def transform(recipe, style="chinese"):
 					ingredient["name"] = sub
 				else:
 					print("no substitutes for", ingredient["name"].lower())
+		elif any(filter(lambda x: x in ingredient["name"].lower(), list(subtree.keys()))):
+			substring_keys = [name for name in list(subtree.keys()) if name in ingredient["name"].lower()]
+			#print("subset names", substring_keys)
+			match = max(substring_keys, key=len)
+			print("found", match)
+
+			ingr_tags = list(getKBSubtree(["ingredients", match]))
+			if style not in ingr_tags:
+				intersect_ingr = {k: len(set(ingr_tags) & set(v)) for k, v in transform_ingr.items()}
+				max_value = max(intersect_ingr.values())
+				max_keys = [k for k, v in intersect_ingr.items() if v == max_value]
+
+				if max_keys and max_value > 0:
+					sub = random.choice(max_keys)
+					print("substitutes for", match, " -> ", sub)
+					subs[ingredient["name"].lower()] = sub
+					ingredient["name"] = sub
+				else:
+					print("swapped", ingredient["name"].lower(), " -> ", match)
+					subs[ingredient["name"].lower()] = match
+					ingredient["name"] = match
+			else:
+				print("swapped", ingredient["name"].lower(), " -> ", match)
+				subs[ingredient["name"].lower()] = match
+				ingredient["name"] = match
+
 		else:
 			print("what is", ingredient["name"].lower(), "?")
 
+	print()
+	print("Direction Swaps:")
 	for idx, direction in enumerate(recipe["directions"]):
 		for original, new_ingr in subs.items():
-			if original in direction:
+			if original in direction.lower():
 				direction = direction.lower().replace(original, new_ingr)
-				print(idx)
+				#print(idx)
 				print(original, " -> ", new_ingr)
-				print()
+				#print()
 
 		recipe["directions"][idx] = direction
 
@@ -121,14 +150,38 @@ def list_ingredients():
 	print(acc)
 
 
+def add_new_ingredients():
+	acc = ""
+	while True:
+
+		name = input("Ingredient name: ")
+		tags = input("Tags: ").split()
+
+		if "exit" in tags:
+			break
+
+		for tag in tags:
+			acc += "addToKB([\"ingredients\", \"{}\", \"{}\"])\n".format(name, tag)
+	print(acc)
+
+
+def actions_from_list(actions):
+	acc = ""
+	for action in actions:
+		acc += "addToKB([\"cooking-actions\", \"{}\"])\n".format(action)
+	print(acc)
+
+
 if __name__ == '__main__':
 	mylist = ['https://www.allrecipes.com/recipe/222000/spaghetti-aglio-e-olio/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%203',
 		      'https://www.allrecipes.com/recipe/258817/chicken-pasta-with-artichoke-hearts/?internalSource=rotd&referringContentType=home%20page&clickId=cardslot%201']
-	test_recipe = make_object(mylist[0])
+	test_recipe = make_object(mylist[1])
 	pp = pprint.PrettyPrinter(indent=4, depth=4)
 	# pp.pprint(test_recipe)
-	print('#######################################################################')
+	print('###################################################################')
 	# print(test_recipe)
 
-	pp.pprint(transform(test_recipe))
-	# list_ingredients()
+	# pp.pprint(transform(test_recipe, "italian"))
+	#list_ingredients()
+	#add_new_ingredients()
+	#actions_from_list(["dab", "trace", "dump", "dash", "unwrap", "turn", "reduce", "mince", "empty", "chill", "serve", "unmold", "cook", "halve", "pound", "smash", "wrap", "blend", "nestle", "unlock", "grate", "increase", "bind", "grind", "break", "flatten", "unseal", "crust", "refrigerate", "overlap", "wiggle", "check", "skim", "whip", "distribute", "criss-cross", "tilt", "spray", "plunge", "tear", "secure", "slit", "sift", "lift", "segment", "punch", "soften", "decrease", "apply", "refill", "tap", "position", "rotate", "center", "temper", "pop", "pulse", "shred", "process", "wash", "melt", "save", "assemble", "flip", "seal", "discard", "swirl", "store", "lubricate", "repeat", "throw", "pin", "strain", "invert", "stream", "mash", "pre-heat", "paint", "rest", "adjust", "extract", "scrub", "clean", "incorporate", "submerge", "crumble", "drain", "thin", "crack", "steep", "dice", "thread", "retain", "pour", "smooth", "garnish", "spoon", "shake", "pat", "rinse", "knead", "flick", "shape", "pass", "light", "zest", "cool", "fan", "scoop", "switch", "splash", "lie", "mop", "close", "peel", "thaw", "add", "without", "even", "alternate", "fermentation", "scatter", "spread", "stuff", "unfold", "fold", "chop", "estimate", "skin", "slide", "uncover", "fluff", "start", "push", "avoid", "ferment", "drop", "remember", "freeze", "coat", "arrange", "dollop", "lay", "finish", "deep", "mist", "trim", "balance", "squeeze", "watch", "puncture", "replace", "split", "portion", "place", "grill", "pierce", "stir", "massage", "taper", "whisk", "separate", "rub", "skewer", "reshape", "double-wrap", "snip", "remove", "slip", "reheat", "brush", "cut", "drape", "release", "debone", "select", "open", "glaze", "oil", "stretch", "scrape", "tamp", "crush", "set", "hollow", "dot", "combine", "wait", "pull", "thicken", "reselect", "round", "frost", "dry", "attach", "soak", "pry", "pack", "wad", "lower", "reposition", "slice", "puree", "loosen", "taste", "stack", "sweeten", "truss", "trim", "husk", "roll", "flake", "keep", "prick", "moisten", "unroll", "wet", "tie", "draw", "pick", "deflate", "mix", "wipe", "dissolve", "cube", "fill", "season", "prepare", "hoisin", "immerse", "tighten", "scramble", "ration", "plop", "use", "pinch", "measure", "decorate", "top", "layer", "dip", "poke", "snap", "grab", "transfer", "divide", "drizzle", "warm", "marinate", "force", "insert", "hold", "slather", "pressure", "braid", "crimp", "sprinkle", "press", "untie", "whirl", "dampen", "fry", "toss", "beat", "sterilize", "twist", "trickle", "cover", "rewarm", "carve", "stick", "rinse"])
